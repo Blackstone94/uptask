5247d2a4b97d785aab5203886e6b64fb5bb6c291
BlackStone94 <eroa94@gmail.com>
2020-10-31 12:29:00 -0600

    399

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+eventsListeners();
+//lista de proyectos
+var listaProyectos=document.querySelector('ul#proyectos');
+function eventsListeners(){
+    //boton para crear proyectos
+    document.querySelector('.crear-proyecto a').addEventListener('click',nuevoProyecto);
+}
+function nuevoProyecto(e){
+    e.preventDefault();
+    var nuevoProyecto=document.createElement('li');
+    nuevoProyecto.innerHTML ='<input type="text" id=nuevo-proyecto>';
+    listaProyectos.appendChild(nuevoProyecto);
+
+    var inputNuevoProyecto=document.querySelector('#nuevo-proyecto');
+    inputNuevoProyecto.addEventListener('keypress',function(e){
+        var tecla=e.which || e.keyCode;
+
+        if(tecla==13){
+            guardarProyectoDb(inputNuevoProyecto.value);
+            listaProyectos.removeChild(nuevoProyecto);
+        }
+    });
+}
+function guardarProyectoDb(nombreProyecto){
+    console.log(nombreProyecto);
+}
